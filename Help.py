699212b0ec59019f7e3bdf0b439480f6b0d8d993
esaleh32 <97734983+esaleh32@users.noreply.github.com>
2022-03-12 20:34:01 +0300

    Update Help.py

--- Help.py
+++ Help.py
@@ -11,7 +11,7 @@ def app():
      st.title('Help page')
      st.write('These apps were programmed as part of my article, namely **"A Comprehensive Evaluation of Existing and New Model-identification approaches for Non-destructive Concrete Strength Assessment"**, submitted to **"Construction and Building materials"** jounral')
 
-     st.subheader('Minimum number of cores')
+     st.subheader('Minimum number of cores app')
      st.write('It allows to estimate the minimum number of cores for reliable estimation of mean concrete strength and concrete variability by defining the five-inputs:')
      st.write('1)Quality of measurements')
      st.write('2)Accepted error in mean estimation')
@@ -20,7 +20,7 @@ def app():
      st.write('5)Coefficient of variation of NDT measurements')
      st.write('The concept of accepted error and risk in the estimation can be illustrated in following figure')
      st.image(image, caption='The concept of CDF used for evaluating the uncertainty (risk) in the estimation ')
-     st.subheader('Conversion models')
+     st.subheader('Conversion models app')
      st.write('The common practice of concrete strength assessment is to combine non-destructive techniques (NDT) with core test measurements to develop a conversion model that is used to estimate the strengths at NDT test locations.') 
 
 
