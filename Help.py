7d3ac42907532e6ee3e41535ef8966a9f62deb9a
esaleh32 <97734983+esaleh32@users.noreply.github.com>
2022-03-12 17:31:58 +0300

    Update Help.py

--- Help.py
+++ Help.py
@@ -8,10 +8,10 @@ import numpy as np
 def app():
      st.write('This app was programmed as part of my article, namely **"A Comprehensive Evaluation of Existing and New Model-identification approaches for Non-destructive Concrete Strength Assessment"**, submitted to **"Construction and Building materials"** jounral')
      st.write('It allows to estimate the minimum number of cores for reliable estimation of mean concrete strength and concrete variability by defining the five-inputs:')
-           st.write('  1)Quality of measurements ')
-               st.write('2)Accepted error in mean estimation;')
-               st.write('3)Accepted error in variability estimation')
-                st.write('4)Accepted risk of wrong estimation')
-                st.write('5)Coefficient of variation of NDT measurements')
+     st.write('1)Quality of measurements')
+     st.write('2)Accepted error in mean estimation;')
+     st.write('3)Accepted error in variability estimation')
+     st.write('4)Accepted risk of wrong estimation')
+     st.write('5)Coefficient of variation of NDT measurements')
 
-    
+
