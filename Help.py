ad948aadeec5bad97da642ed384b096ec1756e54
esaleh32 <97734983+esaleh32@users.noreply.github.com>
2022-03-12 17:20:19 +0300

    Update Help.py

--- Help.py
+++ Help.py
@@ -7,5 +7,6 @@ import pandas as pd
 import numpy as np
 def app():
      st.write('This app was programmed as part of my article, namely **"A Comprehensive Evaluation of Existing and New Model-identification approaches for Non-destructive Concrete Strength Assessment"**, submitted to **"Construction and Building materials"** jounral')
-
+     st.write(\n)
+     st.write('help')
     
