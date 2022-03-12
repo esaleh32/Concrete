d0027c0c3907b67f1715ace54bb1df2103411195
esaleh32 <97734983+esaleh32@users.noreply.github.com>
2022-03-12 16:37:16 +0300

    Update Home2.py

--- Home2.py
+++ Home2.py
@@ -14,6 +14,6 @@ def app():
         **This app estimates the conversion models and minimum number of cores for concrete strength assessment **
         """)
 
-    st.write('Navigate to Minimum number of cores page to estimate the minimum number of cores for concrete strength estimation')
-    st.write ('Navigate to conversion models page to estimate the necessary conversion models for concrete strength estimation')
+    st.write('Navigate to **Minimum number of cores** page to estimate the minimum number of cores for concrete strength estimation')
+    st.write ('Navigate to **Conversion models** page to estimate the necessary conversion models for concrete strength estimation')
     st.write ('Navigate to Help to display the application help system.')
