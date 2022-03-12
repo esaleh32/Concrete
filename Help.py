95462083062e5d3bf94dafb588ff992cadbac673
esaleh32 <97734983+esaleh32@users.noreply.github.com>
2022-03-12 17:38:44 +0300

    Update Help.py

--- Help.py
+++ Help.py
@@ -5,6 +5,8 @@ import numpy as np
 
 import pandas as pd
 import numpy as np
+from PIL import Image
+image = Image.open('image.jpg')
 def app():
      st.write('This app was programmed as part of my article, namely **"A Comprehensive Evaluation of Existing and New Model-identification approaches for Non-destructive Concrete Strength Assessment"**, submitted to **"Construction and Building materials"** jounral')
      st.write('It allows to estimate the minimum number of cores for reliable estimation of mean concrete strength and concrete variability by defining the five-inputs:')
