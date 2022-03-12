c8d043ee997d3a300a8ed9795eca48187a674b12
esaleh32 <97734983+esaleh32@users.noreply.github.com>
2022-03-12 20:31:53 +0300

    Update Help.py

--- Help.py
+++ Help.py
@@ -9,6 +9,7 @@ from PIL import Image
 image = Image.open('image.jpg')
 def app():
      st.title('Help page')
+     st.subheader('Minimum number of cores')
      st.write('This app was programmed as part of my article, namely **"A Comprehensive Evaluation of Existing and New Model-identification approaches for Non-destructive Concrete Strength Assessment"**, submitted to **"Construction and Building materials"** jounral')
      st.write('It allows to estimate the minimum number of cores for reliable estimation of mean concrete strength and concrete variability by defining the five-inputs:')
      st.write('1)Quality of measurements')
@@ -18,6 +19,8 @@ def app():
      st.write('5)Coefficient of variation of NDT measurements')
      st.write('The concept of accepted error and risk in the estimation can be illustrated in following figure')
      st.image(image, caption='The concept of CDF used for evaluating the uncertainty (risk) in the estimation ')
+     st.subheader('Conversion models')
+     st.write('The common practice of concrete strength assessment is to combine non-destructive techniques (NDT) with core test measurements to develop a conversion model that is used to estimate the strengths at NDT test locations.') 
 
 
 
